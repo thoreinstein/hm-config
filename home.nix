14a008b8db5a35f01fcfebf6a75b620ae31014cb
janders <jim_anders@intuit.com>
2024-01-06 00:59:16 -0500

    Add some stuff that won't be needed probably

--- home.nix
+++ home.nix
@@ -26,6 +26,7 @@
     ];
 
     sessionVariables = {
+      BROWSER = "wslview";
       TERM = "xterm-256color";
       FZF_DEFAULT_OPTS="--color=bg+:#313244,bg:#1e1e2e,spinner:#f5e0dc,hl:#f38ba8  --color=fg:#cdd6f4,header:#f38ba8,info:#cba6f7,pointer:#f5e0dc  --color=marker:#f5e0dc,fg+:#cdd6f4,prompt:#cba6f7,hl+:#f38ba8";
     };
@@ -125,6 +126,11 @@
         init = {
           defaultBranch = "main";
         };
+        # url = {
+        #   "git@github.com" = {
+        #     insteadOf = "https://github.com";
+        #   };
+        # };
       };
       hooks = {};
       ignores = [
